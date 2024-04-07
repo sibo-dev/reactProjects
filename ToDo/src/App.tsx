494b1b3c03fda0d48757ff683e822d88da15ae0a
Sibonelo Nkosi <sibonelo.dev@gmail.com>
2024-04-07 12:46:04 +0200

    Working To Do App with React, TypeScript, Vite

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -3,11 +3,14 @@ import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <div>
-      <Toaster position="bottom-center" />
-      <AddTodo />
-      <TodoList />
+    // <div className="flex items-center justify-center h-screen">
+    //   <h1 className="text-5xl font-bold">Todo App Tutorial</h1>
+      <div>
+    <Toaster position="bottom-center" />
+    <AddTodo />
+    <TodoList />
     </div>
+  // </div>
   )
 }
 
--- ToDo/src/components/AddTodo.tsx
+++ ToDo/src/components/AddTodo.tsx
@@ -1,40 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast'
-import { useTodo } from '../context'
+import { useTodo } from '../context/useTodo'
 import { Input } from './Input'
 
 export const AddTodo = () => {
   const [input, setInput] = useState<string>('')
-    /*const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const { addTodo } = useTodo()
 
-    useEffect(() => {
-      if (inputRef.current) {
-        inputRef.current.focus()
-      }
-    },[])
-    */
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+
   const handleSubmission = (e: React.FormEvent) => {
     e.preventDefault()
     if (input.trim() !== '') {
-      setTodos([...todos,input])
+      addTodo(input)
       setInput('')
-    }//console.log('form has been submitted')
+      toast.success('Todo added successfully!')
+    } else {
+      toast.error('Todo field cannot be empty!')
+    }
   }
 
   return (
     <form onSubmit={handleSubmission}>
       <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-        <input
+        <Input
           ref={inputRef}
+          type="text"
+          placeholder="start typing ..."
           value={input}
           onChange={e => setInput(e.target.value)}
-          type="text"
-          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
-          placeholder="start typing ..."
         />
         <button
           type="submit"
-          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-x1"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
         >
           Submit
         </button>
